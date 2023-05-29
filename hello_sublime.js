dacece302faceb9374ca4ddddfa28da9555fa5ff
nihilumex <133829064+nihilumex@users.noreply.github.com>
2023-05-28 22:10:07 -0300

    Add files via upload

---
+++ hello_sublime.js
@@ -0,0 +1,11 @@
+console.log("---------->hello world!<---------");
+
+console.log("(n1+n2) sum code")
+
+var n1 = 25;
+var n2 = 2; 
+sum= n1+n2;
+
+console.log("sum of " + n1 + " and "+ n2 +  " is " + sum +"!" ); 
+
+console.log("-------------->end<---------------");
